449b3ba16570d58613b8dded240221f064fac251
Ignacio Albarrán <ignacioalbarran851@gmail.com>
2021-02-07 19:25:44 +0100

    small refactor

--- snail.rb
+++ snail.rb
@@ -6,10 +6,9 @@ def snail(array)
 
   while !array.empty? do
     array.each{ |row| snail << row.pop }
-    array_to_reverse << array.pop
-    snail << array_to_reverse.flatten.reverse
-    array_to_reverse.clear
+    snail << array.pop.reverse
     array.empty? ? break : array.each{ |row| array_to_reverse.push(row.shift) }
+
     snail << array_to_reverse.reverse
     array_to_reverse.clear
     snail << array.shift
